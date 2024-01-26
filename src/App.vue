602a7b7bb853cbd1a90815ba370b5cb4ade8f2ac
AngelChavarrea <angel.chavarreaj@ug.edu.ec>
2024-01-25 21:24:19 -0500

    se elimino app vue, template html por defecto

--- src/App.vue
+++ src/App.vue
@@ -4,17 +4,7 @@ import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+ <p>Proyecto ToDos</p>
 </template>
 
 <style scoped>
