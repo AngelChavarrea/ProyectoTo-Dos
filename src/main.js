13a4de626bd6336e960041b35e4492412fdb409d
AngelChavarrea <angel.chavarreaj@ug.edu.ec>
2024-01-25 21:40:27 -0500

    se importo componentes de librerias primevue, se agg imagenes y creacion y maquetado de componente login

--- src/main.js
+++ src/main.js
@@ -1,6 +1,61 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
+import PrimeVue from 'primevue/config';
+import { createRouter, createWebHistory } from 'vue-router';
+import 'primevue/resources/primevue.min.css';
+import 'primevue/resources/themes/lara-light-green/theme.css';
+import 'primeflex/primeflex.css';
+import 'primeicons/primeicons.css'; 
 
-createApp(App).mount('#app')
+
+import Button  from 'primevue/button';
+import InputText from 'primevue/inputtext';
+import Toolbar from 'primevue/toolbar';
+import Divider from 'primevue/divider';
+import OrderList from 'primevue/orderlist';
+import Tooltip from 'primevue/tooltip';
+import ConfirmPopup from 'primevue/confirmpopup';
+import ConfirmDialog from 'primevue/confirmdialog';
+import Toast from 'primevue/toast';
+import ConfirmationService from 'primevue/confirmationservice';
+import ToastService from 'primevue/toastservice';
+import DialogService from 'primevue/dialogservice'
+
+import Dialog from 'primevue/dialog';
+
+
+const app = createApp(App);
+
+const routes = [
+    { path: '', component: () => import('./components/login.vue')  },
+    
+];
+  
+  const router = createRouter({
+    history: createWebHistory(),
+    routes,
+  });
+  
+
+app.use(PrimeVue, { ripple: true  });
+app.use(ConfirmationService);
+app.use(ToastService);
+app.use(DialogService);
+app.use(router);
+
+
+app.component('Button', Button );
+app.component('InputText', InputText );
+app.component('Toolbar', Toolbar );
+app.component('Divider', Divider );
+app.component('OrderList', OrderList );
+app.component('ConfirmPopup', ConfirmPopup );
+app.component('ConfirmDialog', ConfirmDialog );
+app.component('Toast', Toast );
+app.component('Dialog', Dialog );
+app.directive('tooltip', Tooltip);
+
+app.mount("#app");
+
+
+export { app };
