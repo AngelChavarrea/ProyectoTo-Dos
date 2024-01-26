8b19374a6cb48cebaf88d3036e9c69bfab6550b2
AngelChavarrea <angel.chavarreaj@ug.edu.ec>
2024-01-26 00:53:30 -0500

    se corrige configuracion para la conpilacion del proyecto

---
+++ src/config.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyCowX3A8wpLzQfAquLhyOnK0nRKKh1FyDM",
+    authDomain: "to-do-new-6da69.firebaseapp.com",
+    projectId: "to-do-new-6da69",
+    storageBucket: "to-do-new-6da69.appspot.com",
+    messagingSenderId: "33875277541",
+    appId: "1:33875277541:web:d3bf049b94bc83cc97060c",
+    measurementId: "G-2X6TV2WZ0X"
+}
